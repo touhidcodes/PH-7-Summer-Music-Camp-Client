f2a0c8ee1c0ad58d02f3ced02ad094ae9e61ad88
Touhidur Zaman <touhidcodes@gmail.com>
2023-06-08 12:13:52 +0600

    pages-router-setup

---
+++ src/routes/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout/MainLayout";
+import Home from "../pages/Home/Home/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <MainLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <div>Error</div>,
+	},
+]);
+
+export default router;
