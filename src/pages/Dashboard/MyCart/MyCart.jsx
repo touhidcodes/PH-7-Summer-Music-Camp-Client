d32c6d0249a108d6e7a57a1f1077d4fd12444af2
Touhidur Zaman <touhidcodes@gmail.com>
2023-06-11 03:44:49 +0600

    delete-cart

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import useCart from "../../hooks/useCart/useCart";
 import { AiOutlineDelete } from "react-icons/ai";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
 	const [cart, refetch] = useCart();
-	console.log(cart);
+
+	const handleDelete = (item) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				fetch(`http://localhost:5000/carts/${item._id}`, {
+					method: "DELETE",
+				})
+					.then((res) => res.json())
+					.then((data) => {
+						if (data.deletedCount > 0) {
+							refetch();
+							Swal.fire("Deleted!", "Your file has been deleted.", "success");
+						}
+					});
+			}
+		});
+	};
 	return (
-		<div className="my-10">
-			<div className="text-xl my-5 font-semibold flex justify-between items-center">
-				<h4 className="text-purple-900">Total Selected Class: {cart.length}</h4>
-                <button className="btn btn-info text-white text-xl">Pay</button>
+		<div className='my-10'>
+			<div className='text-xl my-5 font-semibold flex justify-between items-center'>
+				<h4 className='text-purple-900'>Total Selected Class: {cart.length}</h4>
+				<button className='btn btn-info text-white text-xl'>Pay</button>
 			</div>
 			<div className='overflow-x-auto'>
 				<table className='table'>
@@ -26,13 +51,16 @@ const MyCart = () => {
 					<tbody className='text-xl font-semibold'>
 						{/* row 1 */}
 						{cart.map((row, index) => (
-							<tr>
+							<tr key={row._id}>
 								<th>{index + 1}</th>
 								<td>{row?.class_name}</td>
 								<td>{row?.instructor_name}</td>
 								<td>$ {row?.price}</td>
 								<td>
-									<button className='btn btn-error'>
+									<button
+										className='btn btn-error'
+										onClick={() => handleDelete(row)}
+									>
 										<AiOutlineDelete className='h-8 w-6 text-white' />
 									</button>
 								</td>
